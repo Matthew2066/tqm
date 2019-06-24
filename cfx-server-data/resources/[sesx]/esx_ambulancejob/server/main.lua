db5dec41c31d43a2329853dd5e387447d431486b
Matthew <40185554+Matthew2066@users.noreply.github.com>
2019-06-24 22:37:54 +0100

    EMS JOB

---
+++ cfx-server-data/resources/[sesx]/esx_ambulancejob/server/main.lua
@@ -0,0 +1,88 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterServerEvent('esx_ambulancejob:revive')
+AddEventHandler('esx_ambulancejob:revive', function(target)
+	TriggerClientEvent('esx_ambulancejob:revive', target)
+end)
+
+TriggerEvent('esx_phone:registerNumber', 'ambulance', _('alert_ambulance'), true, true)
+
+ESX.RegisterServerCallback('esx_ambulancejob:removeItemsAfterRPDeath', function(source, cb)
+
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if Config.RemoveCashAfterRPDeath then
+		xPlayer.set('money', 0)
+		xPlayer.setAccountMoney('black_money', 0)
+	end
+
+	if Config.RemoveItemsAfterRPDeath then
+		for i=1, #xPlayer.inventory, 1 do
+			if xPlayer.inventory[i].count > 0 then
+				xPlayer.setInventoryItem(xPlayer.inventory[i].name, 0)
+			end
+		end
+	end
+
+	if Config.RemoveWeaponsAfterRPDeath then
+		for i=1, #xPlayer.loadout, 1 do
+			xPlayer.removeWeapon(xPlayer.loadout[i].name)
+		end
+	end
+
+	cb()
+
+end)
+
+ESX.RegisterServerCallback('esx_ambulancejob:removeItemsAfterRPDeathRemoveMoney', function(source, cb)
+
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if Config.RemoveCashAfterRPDeath then
+		xPlayer.set('money', 0)
+		xPlayer.setAccountMoney('black_money', 0)
+	end
+
+	if Config.EarlyRespawn and Config.EarlyRespawnFine then
+		xPlayer.removeAccountMoney('bank', Config.EarlyRespawnFineAmount)
+	end
+
+	if Config.RemoveItemsAfterRPDeath then
+		for i=1, #xPlayer.inventory, 1 do
+			if xPlayer.inventory[i].count > 0 and not xPlayer.inventory[i].rare then
+				xPlayer.setInventoryItem(xPlayer.inventory[i].name, 0)
+			end
+		end
+	end
+
+	if Config.RemoveWeaponsAfterRPDeath then
+		for i=1, #xPlayer.loadout, 1 do
+			xPlayer.removeWeapon(xPlayer.loadout[i].name)
+		end
+	end
+
+	cb()
+
+end)
+
+ESX.RegisterServerCallback('esx_ambulancejob:getBankMoney', function(source, cb)
+
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local money = xPlayer.getAccount('bank').money
+
+    cb(money)
+end)
+
+TriggerEvent('es:addGroupCommand', 'revive', 'admin', function(source, args, user)
+
+	if args[2] ~= nil then
+		TriggerClientEvent('esx_ambulancejob:revive', tonumber(args[2]))
+	else
+		TriggerClientEvent('esx_ambulancejob:revive', source)
+	end
+
+end, function(source, args, user)
+	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficient Permissions.")
+end, {help = _U('revive_help'), params = {{name = 'id'}}})
