25b9bc9e45377978e707d5921f54da580619c5ca
Matthew <40185554+Matthew2066@users.noreply.github.com>
2019-06-24 01:09:39 +0100

    v menu

--- cfx-server-data/resources/[sesx]/esx_property/config.lua
+++ cfx-server-data/resources/[sesx]/esx_property/config.lua
@@ -6,7 +6,7 @@ Config.RoomMenuMarkerColor    = {r = 102, g = 204, b = 102}
 Config.MarkerType             = 1
 Config.Zones                  = {}
 Config.Properties             = {}
-Config.EnablePlayerManagement = false -- If set to true you need esx_realestateagentjob
+Config.EnablePlayerManagement = true -- If set to true you need esx_realestateagentjob
 Config.Locale                 = 'en'
 
 Config.Properties             = {}
